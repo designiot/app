001ddf0beed3166c47f6e10894ba9f5cfb171c93
Fengda HUANG <h@phodal.com>
2015-11-10 10:42:15 +0800

    add jquery & highchart.js & ng-highchart

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,42 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ["highcharts-ng"])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope, $timeout) {
+  $scope.chartConfig = {
+    title: {
+      text: '月平均气温',
+      x: -20 //center
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+      title: {
+        text: 'Temperature (°C)'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: '°C'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Today',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+    }]
+  }
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function ($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -12,16 +46,16 @@ angular.module('starter.controllers', [])
   //});
 
   $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
+  $scope.remove = function (chat) {
     Chats.remove(chat);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function ($scope) {
   $scope.settings = {
     enableFriends: true
   };
