d8ea511d91132bcb66804ba5be2bf9d561f39cad
Fengda HUANG <h@phodal.com>
2015-11-12 11:23:00 +0800

    Add get data for dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,47 @@
 angular.module('starter.controllers', ["highcharts-ng"])
 
-.controller('DashCtrl', function ($scope, $timeout) {
-  $scope.chartConfig = {
-    title: {
-      text: '月平均气温',
-      x: -20 //center
-    },
-    xAxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-      title: {
-        text: 'Temperature (°C)'
-      },
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-    tooltip: {
-      valueSuffix: '°C'
-    },
-    legend: {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'middle',
-      borderWidth: 0
-    },
-    series: [{
-      name: 'Today',
-      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }]
-  }
+.controller('DashCtrl', function ($scope, $http) {
+    $http.get('http://localhost:3000/api/1/devices/1/results')
+    .then(function(response){
+      console.log(response.data);
+      var data = [];
+      $.each(response.data, function(key, val) {
+        data.push(val.temperature);
+      });
+      $scope.chartConfig = {
+        title: {
+          text: '月平均气温',
+          x: -20 //center
+        },
+        xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+          title: {
+            text: 'Temperature (°C)'
+          },
+          plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+          }]
+        },
+        tooltip: {
+          valueSuffix: '°C'
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle',
+          borderWidth: 0
+        },
+        series: [{
+          name: 'Today',
+          data: data
+        }]
+      }
+    });
 })
 
 .controller('ChatsCtrl', function ($scope, Chats) {
